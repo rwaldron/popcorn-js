3439865e186654ccd4df40c8be58db3a2f77df83
rwldrn <waldron.rick@gmail.com>
2011-01-17 17:08:50 -0500

    [#245] Popcorn data object parser plugin; Parses and loads a Butter generated and serialized data object

--- parsers/parserJSON/popcorn.parserJSON.js
+++ parsers/parserJSON/popcorn.parserJSON.js
@@ -11,7 +11,25 @@
         },
         manifestData = {}, 
         dataObj = data.json.data;
-        
+    
+    
+    /*
+      TODO: add support for filling in source children of the video element
+      
+      
+      remote: [
+        { 
+          src: "whatever.mp4", 
+          type: 'video/mp4; codecs="avc1, mp4a"'
+        }, 
+        { 
+          src: "whatever.ogv", 
+          type: 'video/ogg; codecs="theora, vorbis"'
+        }
+      ]
+
+    */
+    
         
     Popcorn.forEach( dataObj, function ( obj, key ) {
       retObj.data.push( obj );
--- parsers/parserJSON/popcorn.parserJSON.unit.js
+++ parsers/parserJSON/popcorn.parserJSON.unit.js
@@ -1,9 +1,10 @@
 test("Popcorn 0.3 JSON Parser Plugin", function () {
   
-  var expects = 4,
+  var expects = 9,
       count = 0,
       timeOut = 0,
       numLoadingEvents = 5, 
+      trackData,
       trackEvents, 
       interval,
       poppercorn = Popcorn( "#video" );
@@ -22,11 +23,67 @@ test("Popcorn 0.3 JSON Parser Plugin", function () {
   
   stop( 10000 );
   
-  trackEvents = poppercorn.data.trackEvents;
+  
+
+  
+  setTimeout(function () {
+  
+    trackData = poppercorn.data;
+    trackEvents = trackData.trackEvents;
+    
+
+    equals( trackEvents.byStart.length,  numLoadingEvents + 2 , "trackEvents.byStart.length === (5 loaded, 2 padding) " );
+    plus();  
   
   
+    Popcorn.xhr({
+      url: 'data/data.json', 
+      success: function( data ) {
+        
+        var idx = 0;
+        
+        Popcorn.forEach( data.json.data, function (dataObj) {
+          Popcorn.forEach( dataObj, function ( obj, key ) {
+          
+            
+            equals( trackData.history[idx].indexOf(key), 0, "history item '" + trackData.history[idx] + "' matches data key '"+ key+ "' at correct index" );
+            plus();
+            
+            idx++;
+          });
+        });
+        
+        
+      }
+    });
+    
+    
+    
+    
+    poppercorn.listen("timeupdate", function ( event ) {
+
+      if ( Math.round( this.currentTime()) === 3 ) {
+      
+        equals( $("#iframe-container").children().length, 2, '$("#iframe-container").children().length' )
+        plus();
+        equals( $("#map-container").children().length, 1, '$("#map-container").children().length'  );
+        plus();
+        equals( $("#footnote-container").children().length, 2, '$("#footnote-container").children().length'  );
+        plus();
+        
+        this.pause();
+      
+      }
+
+    
+    });
+    
+    
+    poppercorn.currentTime(0).play()
+
   
-  equals( trackEvents.byStart.length,  numLoadingEvents + 2 , "trackEvents.byStart.length === (5 loaded, 2 padding) " );
+  }, 500);
+  
   
   
   
@@ -36,8 +93,8 @@ test("Popcorn 0.3 JSON Parser Plugin", function () {
   
 
   // interval used to wait for data to be parsed
-  interval = setInterval( function() {
-    poppercorn.currentTime(5).play().currentTime(6);
-  }, 2000);
+  //interval = setInterval( function() {
+    //poppercorn.currentTime(5).play().currentTime(6);
+  //}, 2000);
   
 });
