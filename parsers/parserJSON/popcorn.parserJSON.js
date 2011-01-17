8f0ac43eeab7260aa8a6d5788176a001ce28fa1d
rwldrn <waldron.rick@gmail.com>
2011-01-17 14:46:54 -0500

    adding in initial files

---
+++ parsers/parserJSON/popcorn.parserJSON.js
@@ -0,0 +1,21 @@
+// PARSER: 0.3 JSON
+
+(function (Popcorn) {
+  Popcorn.parser( "parseJSON", "JSON", function( data ) {
+
+    // declare needed variables
+    var retObj = {
+          title: "",
+          remote: "",
+          data: []
+        },
+        manifestData = {};
+        
+        
+    console.log(data);        
+
+
+    return retObj;
+  });
+
+})( Popcorn );
